4374e871897f4032d01368e8942052e32df7218a
Stephan <schmid91@mgmail.com>
2020-12-17 19:53:21 -0500

    /

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -14,7 +14,7 @@ function sendEmail(clientfull, subject, message, senderEmail, senderName) {
       html: `Hey, you\'ve triggered a webhook from Directus and sent an email from Netlify Functions<br/>Message:<br/> ${message}`
     }
 
-    clientfull
+    client
       .send(data)
       .then(([response, body]) => {
         fulfill(response)
@@ -45,48 +45,49 @@ exports.handler = function(event, context, callback) {
   const activesend = body.payload.activatesend;
 
 
-    const directus = new directusclient('http://dev.thegovlab.com:8055/');
-         directus.auth.login({ email: DIRECTUS_LOGIN, password: DIRECTUS_PWD }).then( authresp => {
-                 console.log(authresp);
-                directus.items('test').read(body.item).then(response => {
-                  console.log(response);
-                  console.log(response.data);
-                  console.log(response.data.title);
-                  console.log(response.data.content);
-
-                  if(event.queryStringParameters.sgapikey == DIRECTUS_SG_API_KEY && response.data.apikey == DIRECTUS_COLLECTION_API_KEY && activesend == true )
-                  {
-                    const subject = response.data.title;
-                    const message = response.data.content;
-                    console.log(subject);
-                    console.log(message);
-                    console.log(DIRECTUS_SG_API_KEY);
-
-                    client.setApiKey(SENDGRID_API_KEY);
-
-                    const request = {
-          method: 'GET',
-          url: '/v3/marketing/lists'
-        };
-        client.request(request)
-        .then(([response, body]) => {
-          console.log(response.statusCode);
-          console.log(body);
-        })
-                    // sendEmail(
-                    //   client,
-                    //   subject,
-                    //   message,
-                    //   SENDGRID_SENDER_EMAIL,
-                    //   SENDGRID_SENDER_NAME
-                    // )
-                    // .then(response => callback(null, { statusCode: response.statusCode }))
-                    // .catch(err => callback(err, null))
-                  }
-
-
-                });
-            });
+    // const directus = new directusclient('http://dev.thegovlab.com:8055/');
+    //      directus.auth.login({ email: DIRECTUS_LOGIN, password: DIRECTUS_PWD }).then( authresp => {
+    //              console.log(authresp);
+    //             directus.items('test').read(body.item).then(response => {
+    //               console.log(response);
+    //               console.log(response.data);
+    //               console.log(response.data.title);
+    //               console.log(response.data.content);
+    //
+    //               if(event.queryStringParameters.sgapikey == DIRECTUS_SG_API_KEY && response.data.apikey == DIRECTUS_COLLECTION_API_KEY && activesend == true )
+    //               {
+    //                 const subject = response.data.title;
+    //                 const message = response.data.content;
+    //                 console.log(subject);
+    //                 console.log(message);
+    //                 console.log(DIRECTUS_SG_API_KEY);
+    //
+    //                 client.setApiKey(SENDGRID_API_KEY);
+    //
+    //                 const request = {
+    //       method: 'GET',
+    //       url: '/v3/marketing/lists'
+    //     };
+    //
+    //     client.request(request)
+    //     .then(([response, body]) => {
+    //       console.log(response.statusCode);
+    //       console.log(body);
+    //     })
+    //                 // sendEmail(
+    //                 //   client,
+    //                 //   subject,
+    //                 //   message,
+    //                 //   SENDGRID_SENDER_EMAIL,
+    //                 //   SENDGRID_SENDER_NAME
+    //                 // )
+    //                 // .then(response => callback(null, { statusCode: response.statusCode }))
+    //                 // .catch(err => callback(err, null))
+    //               }
+    //
+    //
+    //             });
+    //         });
 
 
   // }
