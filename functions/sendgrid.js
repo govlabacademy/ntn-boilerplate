64a7bc4c90fe9c1f4af5073adfad5935855d69de
Stephan <schmid91@mgmail.com>
2020-12-18 12:19:27 -0500

    /

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -42,13 +42,39 @@ exports.handler = function(event, context, callback) {
   } = process.env
 
   const body = JSON.parse(event.body);
-  const activesend = body.payload.activatesend;
-  console.log(activesend);
 
     const directus = new directusclient('http://dev.thegovlab.com:8055/');
       directus.items('test').read(body.item).then(response => {
         console.log(response);
       })
+
+                      client.setApiKey(SENDGRID_API_KEY);
+
+                      const request = {
+            method: 'GET',
+            url: '/v3/marketing/lists'
+          };
+          client.request(request)
+              .then(([response, body]) => {
+                console.log(response.statusCode);
+                console.log(body);
+              })
+
+  //             var settings = {
+  //   "async": true,
+  //   "crossDomain": true,
+  //   "url": "https://api.sendgrid.com/v3/marketing/singlesends",
+  //   "method": "POST",
+  //   "headers": {
+  //     "authorization": "Bearer "+SENDGRID_API_KEY
+  //   },
+  //   "data": "{\"name\":\"The Govlab\",\"categories\":[\"string\"],\"send_at\":\"string (optional)\",\"send_to\":{\"list_ids\":[\"string\"],\"segment_ids\":[\"string\"],\"all\":\"boolean (optional)\"},\"email_config\":{\"subject\":\"Test me \",\"html_content\":\"string (optional)\",\"plain_content\":\"string (optional)\",\"generate_plain_content\":true,\"design_id\":\"483b855f-7fa9-4967-a7e1-ca05b0676fc7\",\"editor\":\"code\",\"suppression_group_id\":\"integer (optional)\",\"custom_unsubscribe_url\":\"string (optional)\",\"sender_id\":\"integer (optional)\",\"ip_pool\":\"string (optional)\"}}"
+  // }
+  //
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  // });
+
     //      directus.auth.login({ email: DIRECTUS_LOGIN, password: DIRECTUS_PWD }).then( authresp => {
     //              console.log(authresp);
     //             directus.items('test').read(body.item).then(response => {
