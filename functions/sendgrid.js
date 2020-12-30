366d560f596f4d1f78af3663480f1842c644346e
Stephan <schmid91@gmail.com>
2020-12-30 09:32:40 -0500

    /

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -1,4 +1,6 @@
-const directusclient = require('@directus/sdk-js')
+const directusclient = require('@directus/sdk-js');
+
+
 
 // function sendEmail(clientfull, subject, message, senderEmail, senderName) {
 //   return new Promise((fulfill, reject) => {
@@ -29,15 +31,15 @@ exports.handler = function(event, context, callback) {
   // console.log(callback);
   // console.log(event.body);
 
-const contentdata = {
-	"news":[{"article":"First News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read the Story"
-}
-,{
-	"article":"Second News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read Story"
-}
-,{
-	"article":"Third News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read Story"}]
-};
+const contentdata =
+	{	"news":[{"article":"First News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Tadaa Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read the Story"
+	}
+	,{
+		"article":"Second News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read Story"
+	}
+	,{
+		"article":"Third News Story","image":"https://marketing-image-production.s3.amazonaws.com/uploads/d13f90f1285040cd12f86d57ab9e61fee852def9f17e9d65c7a875b213c0fc414235679aa4016327adad5e78758cd9d131db32da4385e226d43fe01cfcd273eb.png","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt elementum sem non luctus. Ut dolor nisl, facilisis non magna quis, elementum ultricies tortor. In mattis, purus ut tincidunt egestas, ligula nulla accumsan justo, vitae bibendum orci ligula id ipsum. Nunc elementum tincidunt libero, in ullamcorper magna volutpat a.","c2a_link":"","c2a_button":"Read Story"}]
+	};
 
   const {
     SENDGRID_API_KEY,
@@ -56,6 +58,7 @@ const contentdata = {
         console.log(response);
       })
 
+			const sgMail = require('@sendgrid/mail')
       const client = require('@sendgrid/client');
 client.setApiKey(SENDGRID_API_KEY);
 // const request = {
@@ -64,31 +67,60 @@ client.setApiKey(SENDGRID_API_KEY);
 //   body: "{\"name\":\"By Stephan\",\"status\":\"scheduled\",\"send_at\":\"2020-12-28T19:48:18Z\",\"sender_id\":\"1274375\",\"send_to\":{\"list_ids\":[\"beb02da9-d59c-4875-8ea7-1797d3a7eb9e\"]},\"email_config\":{\"generate_plain_content\":true,\"design_id\":\"30a18e13-1454-4db5-8217-9676ddd875b4\"}}"
 // };
 
-const request = {
-  method: 'GET',
-  url: '/v3/marketing/lists/2b6695e1-878f-491c-96e5-8c5d655964d9?contact_sample=true'
-  // body: "{\"name\":\"By Stephan\",\"status\":\"scheduled\",\"send_at\":\"2020-12-28T19:48:18Z\",\"sender_id\":\"1274375\",\"send_to\":{\"list_ids\":[\"beb02da9-d59c-4875-8ea7-1797d3a7eb9e\"]},\"email_config\":{\"generate_plain_content\":true,\"design_id\":\"30a18e13-1454-4db5-8217-9676ddd875b4\"}}"
+sgMail.setApiKey(SENDGRID_API_KEY);
+const msg = {
+  to: ['schmid91@gmail.org', 'stephan@thegovlab.org'],
+  from: 'admins@thegovlab.org',
+  subject: 'Hello world',
+  // text: 'Hello plain world!',
+  // html: '<p>Hello HTML world!</p>',
+	dynamic_template_data: contentdata,
+	templateId: 'd-e40c2f0007664c7aac6a8097d9a25300'
 };
-
-client.request(request)
-.then(([response, body]) => {
-  console.log(response.statusCode);
-  console.log(body);
-
-  const request2 = {
-    method: 'PATCH',
-    url: '/v3/marketing/singlesends/30a18e13-1454-4db5-8217-9676ddd875b4/schedule',
-    body: "{\"send_at\":\"2020-12-28T19:48:18Z\"}"
-  };
-
-    client.request(request2)
-    .then(([response2, body2]) => {
-      console.log(response2.statusCode);
-      console.log(response2.body);
+sgMail.sendMultiple(msg).then(response => {
+      console.log(response.statusCode);
+      console.log(response);
     })
+		.catch(error => {
+    // Log friendly error
+    console.error(error);
 
+    if (error.response) {
+      // Extract error msg
+      const {message, code, response} = error;
 
-})
+      // Extract response msg
+      const {headers, body} = response;
+
+      console.error(body);
+    }
+  });;
+
+// const request = {
+//   method: 'GET',
+//   url: '/v3/marketing/lists/2b6695e1-878f-491c-96e5-8c5d655964d9?contact_sample=true'
+//   // body: "{\"name\":\"By Stephan\",\"status\":\"scheduled\",\"send_at\":\"2020-12-28T19:48:18Z\",\"sender_id\":\"1274375\",\"send_to\":{\"list_ids\":[\"beb02da9-d59c-4875-8ea7-1797d3a7eb9e\"]},\"email_config\":{\"generate_plain_content\":true,\"design_id\":\"30a18e13-1454-4db5-8217-9676ddd875b4\"}}"
+// };
+
+// client.request(request)
+// .then(([response, body]) => {
+//   console.log(response.statusCode);
+//   console.log(body);
+//
+//   const request2 = {
+//     method: 'PATCH',
+//     url: '/v3/marketing/singlesends/30a18e13-1454-4db5-8217-9676ddd875b4/schedule',
+//     body: "{\"send_at\":\"2020-12-28T19:48:18Z\"}"
+//   };
+//
+//     client.request(request2)
+//     .then(([response2, body2]) => {
+//       console.log(response2.statusCode);
+//       console.log(response2.body);
+//     })
+//
+//
+// })
 
 
 
