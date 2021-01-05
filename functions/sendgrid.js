e4f29cfc2fb281b2758d7643b90b75e30fc6c70c
Stephan <schmid91@gmail.com>
2021-01-05 09:50:48 -0500

    /

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -70,8 +70,10 @@ client.setApiKey(SENDGRID_API_KEY);
 const requestcount = {
   method: 'PATCH',
   url: '/v3/designs/483b855f-7fa9-4967-a7e1-ca05b0676fc7',
-  body: "{\"name\":\"By Stephan\",\"subject\":\"Happy 2021\"}"
+  body: {"name":"By Stephan","subject":"Happy New 2021"}
 };
+
+
 client.request(requestcount)
 .then(([response, body]) => {
   console.log(response.statusCode);
